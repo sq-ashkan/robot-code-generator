a42b139dbeb59c5a0dd24e5cf667d272a7221b35
ashkan.ghamshi@stud.hawk.de <ashkan.ghamshi@stud.hawk.de>
2023-12-18 12:15:15 +0100

    starting point

---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-10/12 h-full bg-amber-500 bg-opacity-90">
+        <h1 className="p-10">Robotik Code Generator</h1>
+        <div className="w-full flex justify-center items-center">
+          <div className="w-10/12">
+            <hr />
+          </div>
+        </div>
+
+        <div className="w-full flex justify-center items-center">
+          <div className="w-10/12">
+            <div className="w-full h-16 rounded-3xl bg-white mt-10"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
